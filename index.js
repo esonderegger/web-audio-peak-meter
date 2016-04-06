0c58614a934468ebd72cd2b4553aac51fc751a79
Evan Sonderegger <evan.sonderegger@gmail.com>
2016-04-05 23:12:20 -0400

    initial commit of js

---
+++ index.js
@@ -0,0 +1,203 @@
+var webAudioPeakMeter = (function() {
+  'use strict';
+  var options = {
+    borderSize: 2,
+    fontSize: 9,
+    backgroundColor: 'black',
+    tickColor: '#ddd',
+    gradient: ['red 1%', '#ff0 16%', 'lime 45%', '#080 100%'],
+    dbRange: 48,
+    dbTickSize: 6,
+    maskTransition: 'height 0.1s'
+  };
+  var tickWidth;
+  var elementWidth;
+  var elementHeight;
+  var meterHeight;
+  var meterWidth;
+  var meterTop;
+  var vertical = true;
+  var channelCount = 1;
+  var channelMasks = [];
+  var channelPeaks = [];
+  var channelPeakLabels = [];
+
+  var getBaseLog = function(x, y) {
+    return Math.log(y) / Math.log(x);
+  };
+
+  var dbFromFloat = function(floatVal) {
+    return getBaseLog(10, floatVal) * 20;
+  };
+
+  var setOptions = function(userOptions) {
+    for (var k in userOptions) {
+      options[k] = userOptions[k];
+    }
+    tickWidth = options.fontSize * 2.0;
+    meterTop = options.fontSize * 1.5 + options.borderSize;
+  };
+
+  var createMeterNode = function(sourceNode, audioCtx) {
+    var c = sourceNode.channelCount;
+    var meterNode = audioCtx.createScriptProcessor(2048, c, c);
+    sourceNode.connect(meterNode);
+    meterNode.connect(audioCtx.destination);
+    return meterNode;
+  };
+
+  var createContainerDiv = function(parent) {
+    var meterElement = document.createElement('div');
+    meterElement.style.position = 'relative';
+    meterElement.style.width = elementWidth + 'px';
+    meterElement.style.height = elementHeight + 'px';
+    meterElement.style.backgroundColor = options.backgroundColor;
+    parent.appendChild(meterElement);
+    return meterElement;
+  };
+
+  var createMeter = function(domElement, meterNode, optionsOverrides) {
+    setOptions(optionsOverrides);
+    elementWidth = domElement.clientWidth;
+    elementHeight = domElement.clientHeight;
+    var meterElement = createContainerDiv(domElement);
+    if (elementWidth > elementHeight) {
+      vertical = false;
+    }
+    meterHeight = elementHeight - meterTop - options.borderSize;
+    meterWidth = elementWidth - tickWidth - options.borderSize;
+    createTicks(meterElement);
+    createRainbow(meterElement, meterWidth, meterHeight,
+                  meterTop, tickWidth);
+    channelCount = meterNode.channelCount;
+    var channelWidth = meterWidth / channelCount;
+    var channelLeft = tickWidth;
+    for (var i = 0; i < channelCount; i++) {
+      createChannelMask(meterElement, options.borderSize,
+                        meterTop, channelLeft, false);
+      channelMasks[i] = createChannelMask(meterElement, channelWidth,
+                                          meterTop, channelLeft,
+                                          options.maskTransition);
+      channelPeaks[i] = 0.0;
+      channelPeakLabels[i] = createPeakLabel(meterElement, channelWidth,
+                                             channelLeft);
+      channelLeft += channelWidth;
+    }
+    meterNode.onaudioprocess = updateMeter;
+    meterElement.addEventListener('click', function() {
+      for (var i = 0; i < channelCount; i++) {
+        channelPeaks[i] = 0.0;
+        channelPeakLabels[i].textContent = '-∞';
+      }
+    }, false);
+  };
+
+  var createTicks = function(parent) {
+    var numTicks = Math.floor(options.dbRange / options.dbTickSize);
+    var dbTickLabel = 0;
+    var dbTickTop = options.fontSize + options.borderSize;
+    for (var i = 0; i < numTicks; i++) {
+      var dbTick = document.createElement('div');
+      parent.appendChild(dbTick);
+      dbTick.style.width = tickWidth + 'px';
+      dbTick.style.textAlign = 'right';
+      dbTick.style.color = options.tickColor;
+      dbTick.style.fontSize = options.fontSize + 'px';
+      dbTick.style.position = 'absolute';
+      dbTick.style.top = dbTickTop + 'px';
+      dbTick.textContent = dbTickLabel + '';
+      dbTickLabel -= options.dbTickSize;
+      dbTickTop += meterHeight / numTicks;
+    }
+  };
+
+  var createRainbow = function(parent, width, height, top, left) {
+    var rainbow = document.createElement('div');
+    parent.appendChild(rainbow);
+    rainbow.style.width = width + 'px';
+    rainbow.style.height = height + 'px';
+    rainbow.style.position = 'absolute';
+    rainbow.style.top = top + 'px';
+    rainbow.style.left = left + 'px';
+    var gradientStyle = 'linear-gradient(' + options.gradient.join(', ') + ')';
+    rainbow.style.backgroundImage = gradientStyle;
+    return rainbow;
+  };
+
+  var createPeakLabel = function(parent, width, left) {
+    var label = document.createElement('div');
+    parent.appendChild(label);
+    label.style.width = width + 'px';
+    label.style.textAlign = 'center';
+    label.style.color = options.tickColor;
+    label.style.fontSize = options.fontSize + 'px';
+    label.style.position = 'absolute';
+    label.style.top = options.borderSize + 'px';
+    label.style.left = left + 'px';
+    label.textContent = '-∞';
+    return label;
+  };
+
+  var createChannelMask = function(parent, width, top, left, transition) {
+    var channelMask = document.createElement('div');
+    parent.appendChild(channelMask);
+    channelMask.style.width = width + 'px';
+    channelMask.style.height = meterHeight + 'px';
+    channelMask.style.position = 'absolute';
+    channelMask.style.top = top + 'px';
+    channelMask.style.left = left + 'px';
+    channelMask.style.backgroundColor = options.backgroundColor;
+    if (transition) {
+      channelMask.style.transition = options.maskTransition;
+    }
+    return channelMask;
+  };
+
+  var maskSize = function(floatVal) {
+    if (floatVal === 0.0) {
+      return meterHeight;
+    } else {
+      var d = options.dbRange * -1;
+      var returnVal =  Math.floor(dbFromFloat(floatVal) * meterHeight / d);
+      if (returnVal > meterHeight) {
+        return meterHeight;
+      } else {
+        return returnVal;
+      }
+    }
+  };
+
+  var updateMeter = function(audioProcessingEvent) {
+    var inputBuffer = audioProcessingEvent.inputBuffer;
+    var i;
+    var channelData = [];
+    var channelMaxes = [];
+    for (i = 0; i < channelCount; i++) {
+      channelData[i] = inputBuffer.getChannelData(i);
+      channelMaxes[i] = 0.0;
+    }
+    for (var sample = 0; sample < inputBuffer.length; sample++) {
+      for (i = 0; i < channelCount; i++) {
+        if (Math.abs(channelData[i][sample]) > channelMaxes[i]) {
+          channelMaxes[i] = Math.abs(channelData[i][sample]);
+        }
+      }
+    }
+    for (i = 0; i < channelCount; i++) {
+      var thisMaskSize = maskSize(channelMaxes[i], meterHeight);
+      channelMasks[i].style.height = thisMaskSize + 'px';
+      if (channelMaxes[i] > channelPeaks[i]) {
+        channelPeaks[i] = channelMaxes[i];
+        var labelText = dbFromFloat(channelPeaks[i]).toFixed(1);
+        channelPeakLabels[i].textContent = labelText;
+      }
+    }
+  };
+
+  return {
+    createMeterNode: createMeterNode,
+    createMeter: createMeter
+  };
+})();
+
+module.exports = webAudioPeakMeter;
