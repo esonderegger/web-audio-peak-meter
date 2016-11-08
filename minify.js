b09c12687d46bdb0f14810fcaa5244d6ddf7a799
Evan Sonderegger <evan.sonderegger@gmail.com>
2016-11-08 09:54:04 -0500

    add minify script for non-commonJS usage

---
+++ minify.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var UglifyJS = require("uglify-js");
+
+var jsFile = fs.readFileSync('index.js', 'utf8');
+
+var splitLines = jsFile.split('\n');
+var slicedLines = splitLines.slice(0, -2);
+var joinedLines = slicedLines.join('\n');
+
+var uglified = UglifyJS.minify(joinedLines, {fromString: true});
+
+fs.writeFileSync('docs/web-audio-peak-meter.min.js', uglified.code);
